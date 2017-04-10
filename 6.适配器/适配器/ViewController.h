e54e6822422ae69b3f0e2dce5ec0f394e1473b1b
marcus <cjz@xinlonghang.cn>
2017-04-10 16:40:45 +0800

    1、增加适配器模式Demo

---
+++ 6.适配器/适配器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  适配器
+//
+//  Created by marcus on 2017/4/10.
+//  Copyright © 2017年 marcus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
